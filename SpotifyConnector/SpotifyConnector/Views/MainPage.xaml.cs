7e07f3cb4ba6efd37cf2e737db1b740510d43428
Sean Snel <SE.Snel@student.han.nl>
2020-06-22 10:51:43 +0200

    remove unused usings, move views, rename ui project

--- SpotifyConnector/SpotifyConnector/Views/MainPage.xaml.cs
+++ SpotifyConnector/SpotifyConnector/Views/MainPage.xaml.cs
@@ -1,21 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace SpotifyConnector
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ShowPlaylist(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ SpotifyConnector/SpotifyConnector/Views/Playlist.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SpotifyConnector
+{
+    [DesignTimeVisible(false)]
+    public partial class Playlist : ContentPage
+    {
+        public Playlist()
+        {
+            InitializeComponent();
+        }
+    }
+}
